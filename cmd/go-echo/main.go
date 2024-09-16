ed95dfa628e8d33734467b0e4d32f5a5c6832bf3
silverviles <sudarakatharindu2001@gmail.com>
2024-09-16 11:44:59 +0000

    seperate the user roles from the end point names

--- cmd/go-echo/main.go
+++ cmd/go-echo/main.go
@@ -28,18 +28,25 @@ func main() {
 	e.POST("/register", handlers.RegisterUser)
 	e.POST("/login", handlers.LoginUser)
 
-	userGroup := e.Group("/user")
-	userGroup.Use(customMiddleware.CheckRole("user"))
-	userGroup.GET("/products", handlers.GetProducts)
-	userGroup.GET("/products/:id", handlers.GetProductByID)
+	//userGroup := e.Group("/user")
+	//userGroup.Use(customMiddleware.CheckRole("user"))
+	//userGroup.GET("/products", handlers.GetProducts)
+	//userGroup.GET("/products/:id", handlers.GetProductByID)
+	//
+	//adminGroup := e.Group("/admin")
+	//adminGroup.Use(customMiddleware.CheckRole("admin"))
+	//adminGroup.GET("/products", handlers.GetProducts)
+	//adminGroup.GET("/products/:id", handlers.GetProductByID)
+	//adminGroup.POST("/addProducts", handlers.AddProduct)
+	//adminGroup.PUT("/products/:id", handlers.UpdateProduct)
+	//adminGroup.DELETE("/products/:id", handlers.DeleteProduct)
 
-	adminGroup := e.Group("/admin")
-	adminGroup.Use(customMiddleware.CheckRole("admin"))
-	adminGroup.GET("/products", handlers.GetProducts)
-	adminGroup.GET("/products/:id", handlers.GetProductByID)
-	adminGroup.POST("/addProducts", handlers.AddProduct)
-	adminGroup.PUT("/products/:id", handlers.UpdateProduct)
-	adminGroup.DELETE("/products/:id", handlers.DeleteProduct)
+	e.GET("/products", handlers.GetProducts, customMiddleware.CheckRole("user"))
+	e.GET("/products/:id", handlers.GetProductByID, customMiddleware.CheckRole("user"))
+
+	e.POST("/products", handlers.AddProduct, customMiddleware.CheckRole("admin"))
+	e.PUT("/products/:id", handlers.UpdateProduct, customMiddleware.CheckRole("admin"))
+	e.DELETE("/products/:id", handlers.DeleteProduct, customMiddleware.CheckRole("admin"))
 
 	s := &http.Server{
 		Addr: ":8080",
